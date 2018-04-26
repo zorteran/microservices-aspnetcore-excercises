4c33e2459869c0ec839251a81e84e2e937f5f461
zorterab <zorteran@gmail.com>
2018-04-26 12:56:11 -0700

    changed folder, added werkcer.yml

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HelloWorld
 {
